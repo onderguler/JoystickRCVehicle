c9b36299dc6c056d7bb1bdd5b4bc5a2858663045
Onder Guler <onder.glr@hotmail.com>
2024-09-24 22:18:21 +0300

    Initial Commit

---
+++ JoystickRCVehicle/JoystickRCVehicleApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoystickRCVehicleApp.swift
+//  JoystickRCVehicle
+//
+//  Created by Onder Guler on 24.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct JoystickRCVehicleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JoystickRCVehicleTests/JoystickRCVehicleTests.swift
@@ -0,0 +1,17 @@
+//
+//  JoystickRCVehicleTests.swift
+//  JoystickRCVehicleTests
+//
+//  Created by Onder Guler on 24.09.2024.
+//
+
+import Testing
+@testable import JoystickRCVehicle
+
+struct JoystickRCVehicleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
